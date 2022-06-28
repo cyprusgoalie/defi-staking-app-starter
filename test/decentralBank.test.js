793834984c27b0bae3db8e079947ea40bf56148a
cypg <cyprusgoalie@icloud.com>
2022-06-28 20:34:54 +0100

    Restructured file system

---
+++ test/decentralBank.test.js
@@ -0,0 +1,70 @@
+const Tether = artifacts.require("Tether")
+const RWD = artifacts.require("RWD")
+const DecentralBank = artifacts.require("DecentralBank")
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract ('DecentralBank', ([owner, customer]) => {
+    let tether, rwd
+
+    // helper function that converts from Ether to Wei (so I don't have to deal with all of the zeros)
+    function tokens(number){
+        return web3.utils.toWei(number, 'ether')
+    }
+
+    before(async () => {
+        tether = await Tether.new()
+        rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        // Transfer all tokens to DecentralBank (1 million)
+        await rwd.transfer(decentralBank.address, tokens('1000000'))
+
+        // Transfer 100 mock Tethers to customer
+        await tether.transfer(customer, tokens('100'), {from: owner})
+    })
+
+    describe('Mock Tether Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await tether.name()
+            assert.equal(name,'Mock Tether Token')
+        })
+    })
+
+    describe('Reward Token', async () => {
+        it('matches name successfully', async () => {
+            const name = await rwd.name()
+            assert.equal(name,'Reward Token')
+        })
+
+        it('symbol name successfully', async () => {
+            const symbol = await rwd.symbol()
+            assert.equal(symbol,'RWD')
+        })
+    })
+    
+    
+    // describe('Mock Tether Deployment', async () => {
+    //     it('matches name successfully', async () => {
+    //         let tether = await Tether.new()
+    //         const name = await tether.name()
+    //         assert.equal(name,'Mock Tether Token')
+    //     })
+    // })
+
+    // describe('Reward Token', async () => {
+    //     it('matches name successfully', async () => {
+    //         let reward = await RWD.new()
+    //         const name = await reward.name()
+    //         assert.equal(name,'Reward Token')
+    //     })
+
+    //     it('symbol name successfully', async () => {
+    //         let reward = await RWD.new()
+    //         const symbol = await reward.symbol()
+    //         assert.equal(symbol,'RWD')
+    //     })
+    // })
+})
