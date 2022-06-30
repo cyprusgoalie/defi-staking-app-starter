3beb8b5cf4de65d739f73beb347864ffef904c4d
cypg <cyprusgoalie@icloud.com>
2022-06-30 13:44:57 +0100

    Finished Navbar

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class App extends Component {
+    // Our React Code Goes In Here!
+    render() {
+        return (
+            <div>
+                <div className='text-center' style={{
+                    color:'green',
+                    fontSize: '10px'}}>
+                    <h>Hello, World2!</h>
+                </div>
+                <div>
+                    hello
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
+
+// HTML - is the markup language for writing basic text and websites
+// CSS - styles the website (color, fonts, sizes)
+// javascript allows the websites to be dynamic
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+    // Our React Code Goes In Here!
+    render() {
+        return (
+            <div>
+               
+            </div>
+        )
+    }
+}
+
+export default Navbar;
